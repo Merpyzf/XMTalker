5da70ac5a3b714a2a03751a69c5ee6a24eee2b56
wangke <1052060838@qq.com>
2017-06-22 21:59:59 +0800

    主界面布局搭建　未完成

--- app/src/main/java/com/merpyzf/italker/MainActivity.java
+++ app/src/main/java/com/merpyzf/italker/MainActivity.java
@@ -1,60 +1,80 @@
 package com.merpyzf.italker;
 
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.BottomNavigationView;
 import android.util.Log;
 import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.GlideDrawable;
+import com.bumptech.glide.request.animation.GlideAnimation;
+import com.bumptech.glide.request.target.ViewTarget;
 import com.merpyzf.italker.base.BaseActivity;
-import com.merpyzf.italker.widget.recycler.RecyclerAdapter;
+
+import net.qiujuer.genius.ui.widget.FloatActionButton;
 
 import butterknife.BindView;
+import de.hdodenhof.circleimageview.CircleImageView;
+
+
 
 
 public class MainActivity extends BaseActivity {
-    @BindView(R.id.tv_test)
-    TextView tv_test;
+    @BindView(R.id.appbar)
+    AppBarLayout mLyAppBar;
+
+    //toolbar顶部标题栏
+    @BindView(R.id.tv_title)
+    TextView tv_title;
+    //fragment容器
+    @BindView(R.id.fl_container)
+    FrameLayout fl_container;
+    //头像
+    @BindView(R.id.portraitView)
+    CircleImageView portraitView;
+
+    @BindView(R.id.fab_add)
+    FloatActionButton fab_add;
+    //底部导航菜单
+    @BindView(R.id.navigationView)
+    BottomNavigationView navigationView;
+
+
+
 
     @Override
     protected int getContentLayoutId() {
         Log.i("wk","执行了");
+
         return R.layout.activity_main;
     }
 
     @Override
     protected void initWidget() {
+        super.initWidget();
 
-        tv_test.setText("Hello Android");
-        tv_test.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        Glide.with(this)
+                .load(R.drawable.ic_app_bar_bg)
+                .centerCrop()
+                .into(new ViewTarget<View,GlideDrawable>(mLyAppBar) {
+                    @Override
+                    public void onResourceReady(GlideDrawable resource, GlideAnimation<? super GlideDrawable> glideAnimation) {
 
-                Log.i("wk","tag_recycler_holder's id == >"+R.id.tag_recycler_holder);
+                        this.view.setBackground(resource.getCurrent());
 
-            }
-        });
+                    }
+                });
 
 
-        new RecyclerAdapter<String>() {
-            @Override
-            public RecyclerAdapter<String>.MyViewHolder<String> onCreateMyViewHolder(View root, int viewType) {
 
-                return null;
-            }
 
-            @Override
-            protected int getItemViewType(String data, int position) {
-
-                return 0;
-            }
-
-            @Override
-            public void updata(String data, RecyclerAdapter.MyViewHolder viewHolder) {
-
-            }
-        };
 
     }
 
-
-
+    @Override
+    protected void initData() {
+        super.initData();
+    }
 }
